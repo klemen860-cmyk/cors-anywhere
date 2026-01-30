0cce17c496b41e6aac24c5c03d18506b8858cc7a
klemen860-cmyk <klemen860@gmail.com>
2026-01-31 02:16:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var host = process.env.HOST || '0.0.0.0';
-var port = process.env.PORT || 8080;
-
-var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Herkese izin ver
-    requireHeader: [],   // Başlık zorunluluğunu kaldır
-    removeHeaders: ['cookie', 'cookie2'] // Güvenlik için çerezleri temizle
+    originWhitelist: [],
+    requireHeader: [],
+    setHeaders: {
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+        'x-requested-with': 'com.inat.tv.pro'
+    },
+    removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+    console.log('Running CORS Anywhere...');
 });
